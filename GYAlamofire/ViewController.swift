736df1f82209972e04f525fc8293b37fe28de185
airfight <745756619@qq.com>
2018-01-22 20:36:06 +0800

    //

---
+++ GYAlamofire/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GYAlamofire
+//
+//  Created by ForJade Giant on 2018/1/22.
+//  Copyright © 2018年 com.Giant. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Pods/Target Support Files/Pods-GYAlamofire/Pods-GYAlamofire-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_GYAlamofire : NSObject
+@end
+@implementation PodsDummy_Pods_GYAlamofire
+@end
